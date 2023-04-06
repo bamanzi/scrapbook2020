8daac0d7c3d7450792bf04e20a93ae8ae95bffb6
Ba Manzi <ba.manzi@yahoo.com>
2023-04-06 22:07:46 +0800

    add some articles

---
+++ data/20230406215920/index.php
@@ -0,0 +1,44 @@
+/* Code tidied up by ScrapBook */
+.tt1_clearall { display: none; }
+.tt1_clearall + h1, .tt1_clearall + h2, .tt1_clearall + h3, .tt1_clearall + h4, .tt1_clearall + h5, .tt1_clearall + h6 { clear: both; margin-top: 0px; }
+div.thumb { margin-bottom: 0.25em; border-bottom: 1em none; }
+#p-navigation > div.pBody > ul + a { display: block; height: 75px; }
+.tti_buttonlink a { font-size: 185%; border-width: 3px; border-style: solid; -moz-border-top-colors: none; -moz-border-right-colors: none; -moz-border-bottom-colors: none; -moz-border-left-colors: none; border-image: none; display: block; background-color: rgb(238, 238, 238); border-radius: 5pt; padding: 0.1em 0.1em 0.25em; line-height: 0.5em; color: rgb(102, 102, 102); border-color: rgb(204, 204, 204) rgb(153, 153, 153) rgb(153, 153, 153) rgb(204, 204, 204); }
+.tti_buttonlink a:hover { background-color: rgb(249, 249, 249); text-decoration: none; border: 2px solid rgb(153, 153, 153); margin: 1px; }
+.tti_buttonlink a:active { color: rgb(102, 0, 0); }
+.tt1_secondarynav .tti_buttonlink a { font-size: 150%; border-width: 2px; }
+.tt1_secondarynav .tti_buttonlink a:hover { border: 1px solid rgb(153, 153, 153); margin: 1px; }
+.tt1-navigation_franchise_wrapper { margin-top: 0.5em; }
+@media all and (max-width: 500px) {
+  .tt1-navigation_franchise_wrapper td { display: block; }
+  .tt1-navigation_franchise_box { margin-left: auto; margin-right: auto; }
+  .tt1-navigation_franchise_box td { display: table-cell; }
+}
+@media all and (min-width: 500px) {
+  .tt1-navigation_franchise_box { float: right; }
+}
+table.tt1-navigation_franchise { border-style: solid; border-width: 2px 1px; border-color: rgb(204, 204, 204) rgb(153, 153, 153) rgb(153, 153, 153) rgb(204, 204, 204); position: relative; background-color: rgb(238, 238, 238); border-radius: 6px 6px 3px 3px; }
+table.tt1-navigation_franchise > tbody > tr > th { border-spacing: 0px; }
+table.tt1-navigation_franchise > tbody > tr > th.tt1-navigation_franchise-prev, table.tt1-navigation_franchise > tbody > tr > th.tt1-navigation_franchise-next { padding: 2px; width: 20px; }
+table.tt1-navigation_franchise > tbody > tr > th { text-align: center; }
+table.tt1-navigation_franchise > tbody > tr.tt1-navigation_franchise_header { background: rgb(240, 240, 247) url("Meta_titlegloss.png") repeat scroll left top; }
+table.tt1-navigation_franchise { color: rgb(51, 51, 51); }
+table.tt1-navigation_franchise a:link { color: rgb(51, 51, 102); }
+table.tt1-navigation_franchise a:visited { color: rgb(102, 51, 102); }
+table.tt1-navigation_franchise > tbody > tr.tt1-navigation_franchise_header a { color: rgb(119, 119, 119); }
+table.tt1-navigation_franchise > tbody > tr > td ul { margin-right: 1em; margin-top: 0px; }
+table.tt1-navigation_franchise > tbody > tr > td ul > li { list-style: inside url("Meta-list_bullet.png") circle; font-size: smaller; line-height: 1; }
+.tt1_graynote { margin: 0.25em 1.5em; background-color: rgb(239, 239, 239); border: 2px solid rgb(238, 238, 238); padding: 2px; text-align: left; font-style: italic; clear: left; }
+.tt1_graynote::before { content: "Note: "; font-weight: 900; font-style: normal; }
+.tt1_chapters { background-color: rgb(249, 249, 249); border: 1px solid rgb(170, 170, 170); font-size: 95%; padding: 5px 5px 2px; margin-bottom: 0.5em; border-radius: 0.5em; border-spacing: 0px; }
+.tt1_chapters tr.tt1_chapters-header { background-color: rgb(102, 153, 204); margin-bottom: 2em; }
+.tt1_chapters tr.tt1_chapters-header th.tt1_chapters-header-title { padding: 0px 0.5em; }
+.tt1_chapters tr.tt1_chapters-header th.tt1_chapters-header-title, .tt1_chapters tr.tt1_chapters-header th.tt1_chapters-header-title a { color: rgb(255, 255, 255); }
+.tt1_chapters td.tt1_chapters-contents { text-align: center; padding: 4px; }
+.tt1_chapters td.tt1_chapters-contents ul { margin: 0px; }
+.tt1_chapters td.tt1_chapters-contents ul > li { display: inline; }
+.tt1_chapters td.tt1_chapters-contents div.list-header { clear: left; font-weight: 400; display: inline; padding-left: 0.5em; padding-right: 0.5em; margin-right: 0.5em; background-color: rgb(221, 221, 221); }
+.tt1_chapters td.tt1_chapters-contents div.list-header + ul { display: inline; }
+.tt1_chapters tr td ul > li::before { content: "|"; display: inline-block; padding-left: 0.1em; padding-right: 0.35em; }
+.tt1_chapters tr td ul > li:first-child::before { content: ""; display: none; }
+ol.references li { font-size: smaller; }
---
+++ data/20230406215920/load_001.php
@@ -0,0 +1,36 @@
+/* Code tidied up by ScrapBook */
+@import url("index.php");
+h2 { margin-top: 20px; }
+.toc { margin-top: 20px; }
+@media all and (max-width: 500px) {
+}
+@media all and (min-width: 500px) {
+}
+@media all and (min-width: 600px) {
+}
+#n-mainpage { border-bottom: 1px solid rgb(192, 192, 192) ! important; }
+#n-portal { border-top: 1px solid rgb(192, 192, 192) ! important; }
+@media all and (min-width: 600px) {
+}
+@media all and (max-width: 600px) {
+}
+@media all and (min-width: 1000px) {
+}
+@media all and (max-width: 1000px) {
+}
+ul.iconlist { margin-left: 0.5em; }
+li.autobotg1 { background: transparent url("Autobotg1bp.png") no-repeat scroll 0px 1px; padding-left: 18px; list-style: outside none none; }
+li.decepticong1 { background: transparent url("Decepticong1bp.png") no-repeat scroll 0px 1px; padding-left: 18px; list-style: outside none none; }
+li.maximalbw { background: transparent url("Maximalbwbp.png") no-repeat scroll 0px 1px; padding-left: 18px; list-style: outside none none; }
+li.minicon { background: transparent url("Miniconbp.png") no-repeat scroll 0px 1px; padding-left: 18px; list-style: outside none none; }
+#bodyContent a.extiw[href^="http://idwhasbro.shoutwiki.com/"]::after { display: inline-block; content: ""; width: 16px; height: 16px; vertical-align: middle; background: transparent url("IHWikiFavicon.png") no-repeat scroll 0% 0%; }
+@media all and (max-width: 500px) {
+}
+@media all and (min-width: 500px) {
+}
+@media all and (min-width: 600px) {
+}
+@media all and (max-width: 600px) {
+}
+@media print {
+}
